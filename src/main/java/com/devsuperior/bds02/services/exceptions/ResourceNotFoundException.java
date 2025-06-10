1fa89a9325b2e8102cc8d040288c0d4923b67d4c
Gabriel Prata <ghprata7245@gmail.com>
2025-06-10 16:01:50 -0300

    Event update

--- src/main/java/com/devsuperior/bds02/services/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/devsuperior/bds02/services/exceptions/ResourceNotFoundException.java
@@ -1,4 +1,7 @@
 package com.devsuperior.bds02.services.exceptions;
 
-public class ResourceNotFoundException {
+public class ResourceNotFoundException extends RuntimeException{
+    public ResourceNotFoundException(String msg) {
+        super(msg);
+    }
 }
