bb5edd734917bbcfa6a4c14c232b361288d4ffa2
Gabriel Prata <ghprata7245@gmail.com>
2025-06-10 15:32:58 -0300

    City findAll

--- src/main/java/com/devsuperior/bds02/repositories/EventRepository.java
+++ src/main/java/com/devsuperior/bds02/repositories/EventRepository.java
@@ -1,6 +1,6 @@
 package com.devsuperior.bds02.repositories;
 
-import com.devsuperior.bds02.entities.City;
+
 import com.devsuperior.bds02.entities.Event;
 import org.springframework.data.jpa.repository.JpaRepository;
 
